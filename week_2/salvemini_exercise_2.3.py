090ab7bd0b090045c9ce0e2bbbd036b40767e3bf
bsalvemini <bcsalvemini@gmail.com>
2024-06-06 10:55:52 -0400

    Add week_2 folder, add work for Exercise 2.3
    Added the week_2 folder and the Python file for Exercise 2.3 within it

---
+++ week_2/salvemini_exercise_2.3.py
@@ -0,0 +1,12 @@
+"""
+Author: Brandon Salvemini
+Date: 6/4/2024
+File Name: salvemini_exercise_2.3.py
+Description: Prints out a quote
+"""
+# Quote is from: https://www.goodreads.com/quotes/457709-do-your-thing-and-don-t-care-if-they-like-it
+
+# Output quote and author to the screen
+# The \' is used to escape the ' in the word "don't"
+print('"Do your thing and don\'t care if they like it."')
+print("- Tina Fey")
